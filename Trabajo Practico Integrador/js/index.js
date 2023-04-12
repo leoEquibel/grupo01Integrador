bd2cb4d1a6c1be7460ecd421346f4f7a5a934ab1
Alejandro Baudracco <105230509+BaudraccoA@users.noreply.github.com>
2023-04-12 10:52:38 -0300

    se agrega al header dia y hora
    se agrega header del index.html dia y hora y al curso y contacto el target

--- Trabajo Practico Integrador/js/index.js
+++ Trabajo Practico Integrador/js/index.js
@@ -13,3 +13,19 @@ btnHerramientas.addEventListener('click', () => {
   herramientas.scrollIntoView({ behavior: 'smooth' });
 });
 
+// Obtener el elemento para mostrar la fecha y hora actual
+const fechaHora = document.getElementById("fecha-hora");
+		
+// Función para actualizar la fecha y hora cada segundo
+function actualizarFechaHora() {
+  const ahora = new Date();
+  const fecha = ahora.toLocaleDateString();
+  const hora = ahora.toLocaleTimeString();
+  fechaHora.textContent = `${fecha} - ${hora}`;
+}
+
+// Llamar a la función para actualizar la fecha y hora inicialmente
+actualizarFechaHora();
+
+// Llamar a la función cada segundo para actualizar la fecha y hora
+setInterval(actualizarFechaHora, 1000);
