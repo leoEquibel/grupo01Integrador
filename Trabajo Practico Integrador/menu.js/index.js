68154bdb9dc71366b877ce3b3f7ccb7fffa4bfe2
leoEquibel <128635764+leoEquibel@users.noreply.github.com>
2023-04-14 13:50:36 -0300

    alert en menujs
    alert btn cursos menujs

--- Trabajo Practico Integrador/menu.js/index.js
+++ Trabajo Practico Integrador/menu.js/index.js
@@ -2,3 +2,8 @@
 let home;
 home= document.getElementById("home")
 home.addEventListener("click",()=>  alert ("Gracias por su visita!!!"));
+
+let homes;
+homes= document.getElementById("homes")
+homes.addEventListener("click",()=>  alert ("Contactanos Atravez,Del Formulario En Contacto"));
+
